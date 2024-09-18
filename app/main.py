fed3c65f7b2cfcdadbd82fe6818765aafc3dc087
Pavan Kumar <pkumar.rathor@nic.in>
2024-09-18 10:20:20 +0530

    env remove

--- app/main.py
+++ app/main.py
@@ -2,4 +2,4 @@ from app import app
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=3200, reload=True)
